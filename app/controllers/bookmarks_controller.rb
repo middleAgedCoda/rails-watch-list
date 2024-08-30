f0fd87d99f48a19bbb2f7c3a91e4d9f57862e952
samuel chimombe <brentonchimzy2802@gmail.com>
2024-08-30 12:55:38 +0200

    Ruby day 4

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,12 +1,16 @@
 class BookmarksController < ApplicationController
   def new
     @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+    # raise
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
     if @bookmark.save
-    redirect_to bookmarks_path(@bookmark)
+    redirect_to list_path(@list)
     else
       render :new
   end
@@ -21,6 +25,6 @@ end
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:name, :comment)
+    params.require(:bookmark).permit(:movie_id, :comment)
   end
 end
